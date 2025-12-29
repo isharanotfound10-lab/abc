e251ed915837ce3dd5709f3b20d4be705dee61a0
isharanotfound10-lab <isharanotfound10@gmail.com>
2025-12-29 17:41:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'f306e702e4433e62' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '247435e928035c8a' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
